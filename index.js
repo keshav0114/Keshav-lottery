fc7d0ed4b53bbd46a34fe2d95d420389f423e8b6
Keshav <keshavarora0114@gmail.com>
2020-09-21 22:40:01 +0530

    working all

--- index.js
+++ index.js
@@ -8,4 +8,30 @@ function myfunction(){
     console.log(deg);
 
     document.getElementById('box').style.transform ="rotate("+deg+"deg)";
+
+}
+function getinputvalue(){
+    let x=document.getElementById("input1").value;
+   if(x==2)
+   {
+    document.getElementById("lose").style.display="none";
+    document.getElementById("winner").style.display="block";
+   }
+   else if(x==5)
+   {
+    document.getElementById("lose").style.display="none";
+    document.getElementById("winner").style.display="block";
+   }
+   else if(x==7)
+   {
+    document.getElementById("lose").style.display="none";
+    document.getElementById("winner").style.display="block";
+   }
+   else{
+    document.getElementById("winner").style.display="none";   
+    document.getElementById("lose").style.display="block";
+   }
+}
+function reload(){
+    document.innerHTML="RELOAD";
 }
